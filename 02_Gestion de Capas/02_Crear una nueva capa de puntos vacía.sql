e0dab77c03954ea01d165f2ee3b3219ae0f85213
Juan Carlos González González <80714726+geojuanka@users.noreply.github.com>
2021-03-16 08:02:11 +0000

    Update 02_Crear una nueva capa de puntos vacía.sql

--- 02_Gestion de Capas/02_Crear una nueva capa de puntos vacía.sql
+++ 02_Gestion de Capas/02_Crear una nueva capa de puntos vacía.sql
@@ -1,1 +1,1 @@
-create table esquema.nombre_capa (nombre_campo tipo_de_campo, nombre_campo tipo_de_campo, nombre_campo_geometria geometry (point, SRID));
+create table esquema.nombre_capa (nombre_campo tipo_de_campo, nombre_campo tipo_de_campo, nombre_campo_geometria geometry (point, codigo_SRID));
